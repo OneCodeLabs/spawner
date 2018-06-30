bb76ca70101c3827ddbe5e1ab26fff16729d3cd4
goodengineer <gutierrez_nacho@hotmail.com>
2018-06-30 19:56:13 -0300

    added skeleton for ui

--- src/js/main.js
+++ src/js/main.js
@@ -7,13 +7,33 @@ let createGame = () => ({
   board: initBoard()
 })
 
-// game = spawn (game)
-// game = move (0, 3) (1, 3) (game)
-
 window.App = {
   start: () => {
     console.log('app running!')
     this.game = createGame()
+    App.refresh()
+  },
+
+  refresh: () => {
+    const cell = (i, j) => (
+      `<div class='cell' onclick='App.onCellClick(${i},${j})'></div>`
+    )
+    const cells = []
+    const board = this.game.board
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[0].length; j++) {
+        cells.push(cell (i, j))
+      }
+    }
+    document.getElementById('board').innerHTML = cells.join('\n')
+  },
+
+  onCellClick: (i, j) => {
+    console.log('cell clicked! ' + i + ' ' + j);
+  },
+
+  onSpawnClick: () => {
+    console.log('spawn button clicked!');
   }
 }
 
